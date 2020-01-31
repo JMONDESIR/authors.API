f0c5ba10d873f557d9397fe85ddba7b8332ba438
Joel Mondesir <dey6252@hca.corpad.net>
2020-01-31 16:43:47 -0600

    first commit

---
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -0,0 +1,32 @@
+﻿using CourseLibrary.API.Entities;
+using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.Controllers
+{
+    [ApiController]
+    [Route("api")]
+
+    public class AuthorsController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+
+        public AuthorsController(ICourseLibraryRepository courseLibraryRepository )
+        {
+            _courseLibraryRepository = courseLibraryRepository ??
+                throw new ArgumentNullException(nameof(courseLibraryRepository));
+        }
+
+        [HttpGet("authors")]
+        [Produces("application/json", Type = typeof(IEnumerable<Author>))]
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = _courseLibraryRepository.GetAuthors();
+            return new JsonResult(authorsFromRepo);
+        }
+    }
+}
